405e96f0cf57dd3ef84862f8c629f3019664c0d8
domin <domin@DESKTOP-PAHHSBC>
2023-03-03 19:38:24 +0100

    Přidejte soubory projektu.

---
+++ AspSerag/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspSerag.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
